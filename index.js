1a7f3c305c0d2b54be885c6d7bafd4f454fbc341
Sviatoslav <sviatoslavem@gmail.com>
2025-01-29 21:48:52 +0200

    ДЗ 3. Реалізація функції sleep()

--- index.js
+++ index.js
@@ -4,13 +4,24 @@ import PermissionException from "./exceptions/PermissionsException.js";
 
 dotenv.config();
 
-const logger = new Logger();
-const testErr = new PermissionException("Test PermissionException error!");
-const Err = new Error("Test system error!");
-logger.info("Info message");
-logger.warning("Warning message");
-logger.error("Error message");
+// const logger = new Logger();
+// const testErr = new PermissionException("Test PermissionException error!");
+// const Err = new Error("Test system error!");
+// logger.info("Info message");
+// logger.warning("Warning message");
+// logger.error("Error message");
 
-logger.error(testErr);
-logger.warning(testErr);
-logger.error(Err);
+// logger.error(testErr);
+// logger.warning(testErr);
+// logger.error(Err);
+
+async function sleep(ms) {
+  return new Promise((res) => setTimeout(res, ms));
+}
+
+console.log("Початок виконання");
+await sleep(10_000).then(() => {
+  console.log("Дякуємо за очікування!");
+});
+
+console.log("Кінець");
